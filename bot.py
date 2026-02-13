b381b7a30575062a736aaa046d1e2291d5736a2a
vova.zavodyanyy <vova.zavodyanyy@gmail.com>
2026-02-13 10:39:01 +0000

    Fix: Усунуто помилки запуску, пов'язані з БД та ініціалізацією фільтрів

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import logging
 from aiogram import executor
-from loader import dp, bot  # Імпортуємо ОДИН І ТОЙ ЖЕ об'єкт dp
+from loader import dp, bot
 from durak.filters import IsAdminFilter
-
-# Імпортуємо самі роутери з пакету handlers
-from durak.handlers import game, info, gamemode_router
 
 logging.basicConfig(level=logging.INFO)
 
-# Реєстрація фільтра
+# Спочатку реєструємо всі кастомні фільтри
 dp.filters_factory.bind(IsAdminFilter)
 
+# А вже потім імпортуємо обробники, які їх використовують
+from durak.handlers import game, info, gamemode_router
+
 # Реєстрація роутерів
-dp.include(game.router) # Реєструємо ігровий роутер
-dp.include(info.router) # Реєструємо інформаційний роутер
-dp.include(gamemode_router) # Реєструємо роутер для /gamemode
+dp.include(game.router)
+dp.include(info.router)
+dp.include(gamemode_router)
 
 if __name__ == '__main__':
-    # Використовуємо dp з loader для запуску
     executor.start_polling(dp, skip_updates=True)
--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from config import Config, Commands
 from durak.logic.game_manager import GameManager
 from durak.db.database import db
+import os
 
 # 1. Создаем бота
 bot = Bot(token=Config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
@@ -15,7 +16,9 @@ gm.set_bot(bot)  # Передаем экземпляр бота в GameManager
 #Commands = COMMANDS
 
 # Database init
-db.bind('sqlite', 'durak.sqlite', create_db=True)
+# Определяем абсолютный путь к файлу базы данных
+db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'durak.sqlite')
+db.bind('sqlite', db_path, create_db=True)
 db.generate_mapping(create_tables=True)
 
 # Button
