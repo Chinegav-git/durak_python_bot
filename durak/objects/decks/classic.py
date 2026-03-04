1234de7479af99ab9807069da6543ee53042c041
Your Name <you@example.com>
2026-03-04 07:43:11 +0000

    debug

--- durak/objects/decks/classic.py
+++ durak/objects/decks/classic.py
@@ -6,9 +6,12 @@ THEME = {
         'c': 'CAACAgIAAxkBAAEFiF5i9N6ePrpgsVNpA7xD904tUTCjWQACyhoAAlehqUvmgY-dQGLnSikE'
     },
     'SPECIAL': {
-        'draw': 'CAACAgIAAxkBAAEFiGBi9N6gipaKIexU7cVWyxcci2juyAACSx0AArAMqEt3c1jfI8dsHikE',
-        'info': 'CAACAgIAAxkBAAPTaYqSQpE7QBqI6RxQy1V_f7X8AfQAAt-QAAKZOVlI_g93-bxV3SM6BA',
-        'pass': 'CAACAgIAAxkBAAPUaYqSRSdzRqsNYwa_PpuRTyk5pdAAAuGQAAKZOVlIgWO1xu8huGA6BA',
+        # 'draw': 'CAACAgIAAxkBAAEFiGBi9N6gipaKIexU7cVWyxcci2juyAACSx0AArAMqEt3c1jfI8dsHikE',
+        # 'info': 'CAACAgIAAxkBAAPTaYqSQpE7QBqI6RxQy1V_f7X8AfQAAt-QAAKZOVlI_g93-bxV3SM6BA',
+        # 'pass': 'CAACAgIAAxkBAAPUaYqSRSdzRqsNYwa_PpuRTyk5pdAAAuGQAAKZOVlIgWO1xu8huGA6BA',
+        'draw': 'CAACAgIAAxkBAAEFh7pi9NRDcCPWuNKz0LC62J3TM7CGaAACSBkAAt0vkEtZKUcC8YvNVSkE',
+        'info': 'CAACAgIAAxkBAAEFh7pi9NRDcCPWuNKz0LC62J3TM7CGaAACSBkAAt0vkEtZKUcC8YvNVSkE',
+        'pass': 'CAACAgIAAxkBAAEFh7pi9NRDcCPWuNKz0LC62J3TM7CGaAACSBkAAt0vkEtZKUcC8YvNVSkE',
     },
     'normal': {
 
